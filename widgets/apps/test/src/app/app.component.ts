3cf9b5ab6192728b2a240f399a59af2333cac9bc
mchukhlov <mchukhlov@beeline.ru>
2021-08-02 13:47:09 +0300

    npx create-nx-workspace@latest

---
+++ widgets/apps/test/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'widgets-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'test';
+}
